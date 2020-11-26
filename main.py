8a731ee7b03670a6f644f03515e6d98e2373f287
SabaHilal <57343679+SabaHilal@users.noreply.github.com>
2020-11-26 18:38:10 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,111 @@
 from flask import Flask, jsonify, request
-from csv import reader
 
-all_movies = []
+from storage import all_movies, liked_movies, not_liked_movies, did_not_watch
+from demographic_filtering import output
+from content_filtering import get_recommendations
 
+import os, ssl, time
 
-# read csv file as a list of lists
-with open('movies.csv',  encoding="utf8") as read_obj:
-    # pass the file object to reader() to get the reader object
-    csv_reader = reader(read_obj)
-    # Pass reader object to list() to get a list of lists
-    data = list(csv_reader)
-    print(data[1])
-    all_movies = data[1:]
+#Setting an HTTPS Context to fetch data 
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+    getattr(ssl, '_create_unverified_context', None)): 
+    ssl._create_default_https_context = ssl._create_unverified_context
 
-
-liked_movies = []
-not_liked_movies = []
-did_not_watch = []
+print(len(liked_movies))
 
 app = Flask(__name__)
 
-
 @app.route("/get-movie")
 def get_movie():
-   return jsonify({
-       "data": all_movies[0],
-       "status": "success"
-   })
+    movie_data = {
+        "title": all_movies[0][19],
+        "poster_link": all_movies[0][27],
+        "release_date": all_movies[0][13] or "N/A",
+        "duration": all_movies[0][15],
+        "rating": all_movies[0][20],
+        "overview": all_movies[0][9]
+    }
+    return jsonify({
+        "data": movie_data,
+        "status": "success"
+    })
 
 @app.route("/liked-movie", methods=["POST"])
 def liked_movie():
     movie = all_movies[0]
-    all_movies = all_movies[1:]
     liked_movies.append(movie)
+    all_movies.pop(0)
     return jsonify({
-        "status": "success"
+        "status": "success",
+        "total liked": len(liked_movies),
+        "total_movies_now":len(all_movies),
     }), 201
 
 @app.route("/unliked-movie", methods=["POST"])
 def unliked_movie():
     movie = all_movies[0]
-    all_movies = all_movies[1:]
     not_liked_movies.append(movie)
+    all_movies.pop(0)
     return jsonify({
-        "status": "success"
+        "status": "success",
+        "total unliked": len(not_liked_movies),
+        "total_movies_now":len(all_movies),
     }), 201
 
 @app.route("/did-not-watch", methods=["POST"])
-def did_not_watch():
+def did_not_watch_view():
     movie = all_movies[0]
-    all_movies = all_movies[1:]
     did_not_watch.append(movie)
+    all_movies.pop(0)
     return jsonify({
-        "status": "success"
+        "status": "success",
+        "total movies not watched": len(did_not_watch),
+        "total_movies_now":len(all_movies),
     }), 201
 
+@app.route("/popular-movies")
+def popular_movies():
+    movie_data = []
+    for movie in output:
+        _d = {
+            "title": movie[0],
+            "poster_link": movie[1],
+            "release_date": movie[2] or "N/A",
+            "duration": movie[3],
+            "rating": movie[4],
+            "overview": movie[5]
+        }
+        movie_data.append(_d)
+    return jsonify({
+        "data": movie_data,
+        "status": "success"
+    }), 200
+
+@app.route("/recommended-movies")
+def recommended_movies():
+    all_recommended = []
+    for liked_movie in liked_movies:
+        output = get_recommendations(liked_movie[19])
+        for data in output:
+            all_recommended.append(data)
+    import itertools
+    all_recommended.sort()
+    all_recommended = list(all_recommended for all_recommended,_ in itertools.groupby(all_recommended))
+    movie_data = []
+    for recommended in all_recommended:
+        _d = {
+            "title": recommended[0],
+            "poster_link": recommended[1],
+            "release_date": recommended[2] or "N/A",
+            "duration": recommended[3],
+            "rating": recommended[4],
+            "overview": recommended[5]
+        }
+        movie_data.append(_d)
+    return jsonify({
+        "data": movie_data,
+        "status": "success"
+    }), 200
+
 if __name__ == "__main__":
-  app.run()
+    app.run(debug=True)
--- storage.py
+++ storage.py
@@ -1,12 +1,16 @@
-import csv
+from csv import reader
 
 all_movies = []
 
-with open('final.csv') as f:
-    reader = csv.reader(f)
-    data = list(reader)
+with open('final.csv', encoding="utf8") as read_obj:
+    csv_reader = reader(read_obj)
+    data = list(csv_reader)
     all_movies = data[1:]
+    print(data[1])
 
 liked_movies = []
 not_liked_movies = []
 did_not_watch = []
+
+
+
